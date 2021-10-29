def479647a4b04c5f3e34a957f779e2bf7d8e309
Abdallah <abdallah.m@padoa-group.com>
2021-10-29 12:42:20 +0200

    test

--- update-check.js
+++ update-check.js
@@ -1,33 +1,20 @@
-var AutoUpdater = require('auto-updater');
- 
-    var autoupdater = new AutoUpdater({
-     autoupdate: false,
-     checkgit: true,
-     contenthost: 'https://github.com/MeddahAbdellah/test-auto-update.git',
-    });
- 
-    autoupdater.on('check.out-dated', function(v_old, v) {
-      console.warn("Your version is outdated. " + v_old + " of " + v);
-      autoupdater.fire('download-update'); // If autoupdate: false, you'll have to do this manually.
-      // Maybe ask if the'd like to download the update.
-    });
-    autoupdater.on('update.downloaded', function() {
-      console.log("Update downloaded and ready for install");
-      autoupdater.fire('extract'); // If autoupdate: false, you'll have to do this manually.
-    });
-    autoupdater.on('update.not-installed', function() {
-      console.log("The Update was already in your folder! It's read for install");
-      autoupdater.fire('extract'); // If autoupdate: false, you'll have to do this manually.
-    });
-    autoupdater.on('update.extracted', function() {
-      console.log("Update extracted successfully!");
-      console.warn("RESTART THE APP!");
-    });
- 
-    // Start checking
-    
+var http = require('http')
+var createHandler = require('github-webhook-handler')
+var handler = createHandler({ path: 'https://my-app-abdallah.com/update', secret: '' })
 
-    setInterval(() => {
-        autoupdater.fire('check');
-        console.log('checking Updates');
-    }, 5000);
+http.createServer(function (req, res) {
+  handler(req, res, function (err) {
+    res.statusCode = 404
+    res.end('no such location')
+  })
+}).listen(7777)
+
+handler.on('error', function (err) {
+  console.error('Error:', err.message)
+})
+
+handler.on('push', function (event) {
+  console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref)
+})
